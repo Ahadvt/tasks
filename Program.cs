b0a9d1e17e720ee9b498c4d2a3f8d380bbc8fbbe
Ahadvt <90691214+Ahadvt@users.noreply.github.com>
2021-09-23 02:18:23 +0400

    Add files via upload
    aded first task in c#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOFdoor = 300;
+            int widthOfdoor = 200;
+            int lengthOfwardrob = 200;
+            int widththOfwarrob = 100;
+            int sideLeftTop = 50;
+
+            if (lengthOFdoor > lengthOfwardrob && widthOfdoor > widththOfwarrob)
+            {
+                Console.WriteLine("true");
+            }else if (lengthOFdoor>lengthOfwardrob&&widthOfdoor>sideLeftTop)
+            {
+                Console.WriteLine("true");
+            }else if (lengthOFdoor > sideLeftTop && widthOfdoor > widththOfwarrob)
+            {
+                Console.WriteLine("true");
+            }else if (lengthOFdoor > widththOfwarrob && widthOfdoor > sideLeftTop)
+            {
+                Console.WriteLine("true");
+            }else if (lengthOFdoor > widththOfwarrob && widthOfdoor > sideLeftTop)
+            {
+                Console.WriteLine("true");
+            }else if (lengthOFdoor > sideLeftTop && widthOfdoor > lengthOFdoor)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+    }
+}
