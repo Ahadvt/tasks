188b89b9068855d1dde648d034e5c41bdab60e1d
Ahadvt <90691214+Ahadvt@users.noreply.github.com>
2021-09-23 04:08:42 +0400

    Add files via upload

---
+++ taskc#.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1den 10a qeder olan tek ededlerin hasilini tapin
+            int n = 10;
+            int i = 0;
+            int h = 1;
+
+            while (i < n)
+            {
+                i++;
+                if (i % 2 != 0)
+                {
+                    h *= i;
+                }
+                
+                   
+            }
+
+            Console.WriteLine(h);
+        }
+    }
+}
