188d0dcae181b0c76c536192bc4080fe6dd4657a
Ahadvt <90691214+Ahadvt@users.noreply.github.com>
2021-09-25 04:11:27 +0400

    Add files via upload

---
+++ Arrytask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ;
+
+            Console.WriteLine(BooLean(100, 200, 300));
+        }
+
+       static bool BooLean(params int[] arr)
+        {
+            bool Check = true;
+            int n = 3000;
+            foreach (int item in arr)
+            {
+                if (n==item)
+                {
+                    return Check;
+                }
+            }
+            return false;
+
+
+
+
+
+        }
+        
+    }
+}
