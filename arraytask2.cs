90b49160c6d6edf3ec5cfd7085a4035a9ae7ac92
Ahadvt <90691214+Ahadvt@users.noreply.github.com>
2021-09-27 02:43:10 +0400

    Add files via upload

---
+++ arraytask2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string arr = Console.ReadLine();
+
+            Compare(int.Parse(arr));
+           
+        }
+
+        static void Compare( params int[] arr)
+        {
+
+            int eb = 0;
+            
+            for (int i = 0; i<100; i++)
+                
+            {
+                if (arr[0] > eb)
+                {
+                    eb = arr[i];
+                }
+
+            }
+            Console.WriteLine("en boyuk massiv"+"   "+eb);          
+            
+            
+            
+
+        }
+
+    }
+}
