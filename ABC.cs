5d693b247088e16ffee949f6505ef410d12fe0f8
Ahadvt <90691214+Ahadvt@users.noreply.github.com>
2021-10-13 04:13:04 +0400

    Add files via upload

---
+++ ABC.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class ABC
+    {
+        public int Number;
+        public int Count;
+
+        public ABC(int number)
+        {
+            Number = number;
+        }
+
+        public event Action<bool> Test;
+
+        public void Gettest()
+        {
+            for (int i =2; i<=9; i++)
+            {
+                if (Number % i == 0)
+                {
+                    Count++;
+
+                    if (Count > 2)
+                    {
+                        Test(true);
+                        break;
+                    }
+                    else
+                    {
+                        Test(false);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
---
+++ PProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ABC abc = new ABC(7);
+
+
+            abc.Test += delegate (bool m)
+              {
+                  if (m)
+                  {
+                      Console.WriteLine("murekkeb ededir");
+                  }
+                  else
+                  {
+                      Console.WriteLine("sade ededdir");
+                  }
+
+              };
+            abc.Gettest();
+            
+        }
+    }
+}
